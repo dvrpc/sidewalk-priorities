703fdce6d919848b34d20d6fe748761fa3122054
aaronfraint <aaronfraint@icloud.com>
2021-06-25 14:24:52 -0400

    add click event for POI layer

---
+++ src/js/clicks.js
@@ -0,0 +1,28 @@
+import { sw_filter, osm_filter } from "./layers.js";
+
+const poi_click = (map) => {
+  map.on("click", "all_pois", function (e) {
+    var props = e.features[0].properties;
+
+    var id_filter = ["in", "eta_uid", props.eta_uid];
+    var id_filter_as_string = ["in", "eta_uid", props.eta_uid.toString()];
+
+    map.setFilter("iso_sw", ["all", id_filter_as_string, sw_filter]);
+    map.setPaintProperty("iso_sw", "fill-opacity", 0.7);
+
+    map.setFilter("iso_osm", ["all", id_filter_as_string, osm_filter]);
+    map.setPaintProperty("iso_osm", "fill-opacity", 0.7);
+
+    map.setFilter("selected_poi", id_filter);
+    // map.setPaintProperty("selected_poi", "circle-opacity", 1);
+    map.setPaintProperty("selected_poi", "circle-stroke-opacity", 1);
+
+    map.flyTo({
+      center: e.lngLat,
+      zoom: 13,
+      essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+    });
+  });
+};
+
+export { poi_click };
