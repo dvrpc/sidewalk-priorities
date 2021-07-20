a86757a0c760a5cd548785fb158ae1b01a6fefb4
aaronfraint <aaronfraint@icloud.com>
2021-07-20 16:57:06 -0400

    update order and refresh API data on click

--- src/js/clicks.js
+++ src/js/clicks.js
@@ -1,5 +1,6 @@
 import { sw_filter, osm_filter } from "./layers.js";
 import { bindPopup, poi_message } from "./popup";
+import { reloadGeojson } from "./api.js";
 
 const poi_click = (map) => {
   map.on("click", "all_pois", function (e) {
@@ -8,15 +9,12 @@ const poi_click = (map) => {
     var id_filter = ["in", "eta_uid", props.eta_uid];
     var id_filter_as_string = ["in", "eta_uid", props.eta_uid.toString()];
 
+    // Get links within walkshed from API
+    reloadGeojson(map, props.eta_uid);
+
     map.setFilter("iso_sw", ["all", id_filter_as_string, sw_filter]);
-    map.setPaintProperty("iso_sw", "fill-opacity", 0.4);
-
     map.setFilter("iso_osm", ["all", id_filter_as_string, osm_filter]);
-    map.setPaintProperty("iso_osm", "fill-opacity", 0.2);
-
     map.setFilter("selected_poi", id_filter);
-    // map.setPaintProperty("selected_poi", "circle-opacity", 1);
-    map.setPaintProperty("selected_poi", "circle-stroke-opacity", 1);
 
     map.flyTo({
       center: e.lngLat,
@@ -27,8 +25,18 @@ const poi_click = (map) => {
     // Make sure the user's cursor is no longer the pointer finger
     map.getCanvas().style.cursor = "";
 
+    // Show the popup
     var msg = poi_message(e);
     bindPopup(map, msg, e);
+
+    // Allow the filtered layers to appear
+    map.setPaintProperty("iso_sw", "fill-opacity", 0.4);
+    map.setPaintProperty("iso_osm", "fill-opacity", 0.2);
+    map.setPaintProperty("selected_poi", "circle-stroke-opacity", 1);
+
+    // Show the distance selection slider
+    var sliderbox = document.getElementById("slider-box");
+    sliderbox.style.setProperty("visibility", "visible");
   });
 };
 
