fb5823253f76e02f36a83fd061ed675698e2b852
aaronfraint <aaronfraint@icloud.com>
2021-09-14 12:05:46 -0400

    add click event for POI

--- src/js/municipal_map/clicks.js
+++ src/js/municipal_map/clicks.js
@@ -1,20 +1,8 @@
-import { sw_filter, osm_filter } from "./layers.js";
-import { reloadGeojson } from "./api.js";
-import { title_cased_text, convert_ratio_to_text } from "./text.js";
-import { update_graph_with_api_data } from "./graph.js";
 import { api_url_base } from "./api.js";
 
 const gap_click = (map) => {
   /**
-   * Handle click events for the 'all_pois' layer
-   * Steps after user clicks:
-   *  - reload missing links layer via API for this POI ID
-   *  - filter tileset layers for this POI ID
-   *  - zoom in to the clicked point
-   *  - populate the sidebar div with related text info
-   *  - set visibility properties on necessary layers and divs so that they appear
-   *
-   * @param {mapboxgl.Map} map - The map object for the page
+   * Handle click events for the 'gap-layer'
    */
   map.on("click", "gap-layer", function (e) {
     var props = e.features[0].properties;
@@ -24,21 +12,6 @@ const gap_click = (map) => {
     var id_filter = ["in", "uid", props.uid];
     console.log(id_filter);
     map.setFilter("clicked-gap", id_filter);
-    // var iso_filter = ["in", "eta_uid", props.poi_uid.toString()];
-
-    // console.log(iso_filter);
-
-    // // Get links within walkshed from API
-    // reloadGeojson(map, props.poi_uid);
-
-    // update_graph_with_api_data(graph, props.poi_uid);
-
-    // map.setFilter("iso_sw", ["all", iso_filter, sw_filter]);
-    // map.setFilter("iso_sw_outline", ["all", iso_filter, sw_filter]);
-    // map.setFilter("iso_osm", ["all", iso_filter, osm_filter]);
-    // map.setFilter("iso_osm_outline", ["all", iso_filter, osm_filter]);
-
-    // map.setFilter("selected_poi", id_filter);
 
     map.flyTo({
       center: e.lngLat,
@@ -86,30 +59,36 @@ const gap_click = (map) => {
     };
     request.send();
 
-    // // Make sure the user's cursor is no longer the pointer finger
-    // map.getCanvas().style.cursor = "";
-
-    // var poiRatio = document.getElementById("selected-poi-ratio");
-    // poiRatio.innerHTML =
-    //   convert_ratio_to_text(props.ab_ratio) + " sidewalk coverage";
-
-    // // Allow the filtered layers to appear
-    // map.setPaintProperty("iso_sw", "fill-opacity", 0.7);
-    // map.setPaintProperty("iso_osm", "fill-opacity", 0.7);
-
-    // map.setPaintProperty("iso_sw_outline", "line-opacity", 0.9);
-    // map.setPaintProperty("iso_osm_outline", "line-opacity", 0.9);
-
-    // map.setPaintProperty("selected_poi", "circle-stroke-opacity", 0.5);
-
     // Show the info box for this POI
     var infoBox = document.getElementById("stat-box");
     infoBox.style.setProperty("visibility", "visible");
-
-    // // Show the secondary legend
-    // var legend = document.getElementById("walkshed-legend");
-    // legend.style.setProperty("display", "inline");
   });
 };
 
-export { gap_click };
+const poi_click = (map) => {
+  map.on("click", "all_pois", function (e) {
+    var props = e.features[0].properties;
+    console.log(props);
+    console.log(e);
+
+    var lat = e.lngLat.lat;
+    var lng = e.lngLat.lng;
+
+    let url =
+      "/by-destination.html?lat=" +
+      lat +
+      "&lng=" +
+      lng +
+      "&id=" +
+      props.poi_uid +
+      "&name=" +
+      props.poi_name.replace(" ", "_") +
+      "&ab_ratio=" +
+      props.ab_ratio.toString();
+
+    console.log(url);
+    window.location.href = url;
+  });
+};
+
+export { gap_click, poi_click };
