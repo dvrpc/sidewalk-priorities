c85ad0f75d168c93cf5c3d92151a3c38a0fbbd0b
aaronfraint <aaronfraint@icloud.com>
2021-10-05 10:41:37 -0400

    clean up, add handling for clicking on existing sw

--- src/js/map/clicks.js
+++ src/js/map/clicks.js
@@ -1,15 +1,10 @@
-import { sw_filter, osm_filter } from "./layers.js";
 import {
   reload_gaps_near_poi,
   reload_pois_near_gap,
   reload_pois_near_sw,
 } from "./api.js";
-import { title_cased_text, convert_ratio_to_text } from "./text.js";
-import { update_graph_with_api_data } from "./graph.js";
 import {
-  hide_extra_ui_boxes,
   hide_osm_isos,
-  hide_layers,
   show_element_inline,
   show_element,
   hide_element,
@@ -17,21 +12,22 @@ import {
   hide_layers_from_gap_selection,
   filter_osm_iso_layers_by_id,
   filter_poi_layers_by_id,
-  show_osm_iso_layers,
   show_list_item,
 } from "./switches.js";
-import { sw_zoom_threshold } from "./hover.js";
+import { ZOOM_THRESHOLD_FOR_LINE_INTERACTION } from "./hover.js";
+import { update_graph_with_api_data } from "./graph.js";
+import { add_marker_to_map, remove_markers } from "./markers.js";
+import { title_cased_text, convert_ratio_to_text } from "./text.js";
+
+// CLICK LOGIC
+// -----------
 
 const gap_click_logic = (map, uid, map_center, island_count) => {
   var id_filter = ["in", "uid", uid];
 
   map.setFilter("clicked_gap", id_filter);
 
-  map.flyTo({
-    center: map_center,
-    zoom: 13,
-    essential: true,
-  });
+  add_marker_to_map(map, map_center);
 
   hide_osm_isos(map);
   map.setFilter("selected_poi", ["==", "type", "none"]);
@@ -69,7 +65,7 @@ const gap_click_logic = (map, uid, map_center, island_count) => {
   ]);
 
   map.setPaintProperty("clicked_gap", "line-opacity", 0.8);
-
+  map.setPaintProperty("selected_existing_sidewalk", "line-opacity", 0);
   var muni_dropdown = document.getElementById("dropdown_category");
   muni_dropdown.value = "all";
 
@@ -84,6 +80,18 @@ const poi_click_logic = (
   poi_name,
   ab_ratio
 ) => {
+  /**
+   * Handle click events for the 'all_pois' layer
+   * Steps after user clicks:
+   *  - reload missing links layer via API for this POI ID
+   *  - filter tileset layers for this POI ID
+   *  - zoom in to the clicked point
+   *  - populate the sidebar div with related text info
+   *  - set visibility properties on necessary layers and divs so that they appear
+   *
+   * @param {mapboxgl.Map} map - The map object for the page
+   */
+
   hide_layers_from_gap_selection(map);
 
   var poi_uid = parseInt(poi_uid);
@@ -96,12 +104,6 @@ const poi_click_logic = (
 
   filter_poi_layers_by_id(map, poi_uid);
 
-  map.flyTo({
-    center: map_center,
-    zoom: 14,
-    essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-  });
-
   // Make sure the user's cursor is no longer the pointer finger
   map.getCanvas().style.cursor = "";
 
@@ -112,7 +114,9 @@ const poi_click_logic = (
   let ratio_text = convert_ratio_to_text(ab_ratio) + " sidewalk coverage";
   set_text_to_div(ratio_text, "selected-poi-ratio");
 
-  map.setPaintProperty("selected_poi", "circle-stroke-opacity", 1);
+  map.setPaintProperty("selected_poi", "circle-stroke-opacity", 0.8);
+  map.setPaintProperty("selected_existing_sidewalk", "line-opacity", 0);
+  remove_markers();
 
   show_element("info-box");
   show_element("walkshed-legend");
@@ -122,18 +126,49 @@ const poi_click_logic = (
   show_element_inline("text-about-gap-click");
 };
 
+const sw_click_logic = (map, e) => {
+  add_marker_to_map(map, e.lngLat);
+
+  // Update the selected sidewalk layer with this geometry
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ["sw"],
+  });
+
+  map
+    .getSource("selected_existing_sidewalk_data")
+    .setData(features[0].geometry);
+
+  // Make it visible
+  map.setPaintProperty("selected_existing_sidewalk", "line-opacity", 1);
+
+  reload_pois_near_sw(map, e.lngLat);
+
+  hide_osm_isos(map);
+  map.setFilter("selected_poi", ["==", "type", "none"]);
+
+  map.setPaintProperty("clicked_gap", "line-opacity", 0);
+
+  map.setFilter("selected_poi_entrypoints", [
+    "==",
+    "type",
+    "none - this filter should return zero results",
+  ]);
+
+  set_text_to_div("This existing sidewalk:", "gap-header");
+
+  hide_element("stat-island");
+
+  set_text_to_div("Around an Existing Sidewalk", "subtitle");
+
+  show_element("stat-box");
+  hide_element("info-box");
+  hide_element("walkshed-legend");
+  show_element("selected-legend");
+};
+
+// CLICK EVENTS
+// ------------
 const poi_click = (map, graph) => {
-  /**
-   * Handle click events for the 'all_pois' layer
-   * Steps after user clicks:
-   *  - reload missing links layer via API for this POI ID
-   *  - filter tileset layers for this POI ID
-   *  - zoom in to the clicked point
-   *  - populate the sidebar div with related text info
-   *  - set visibility properties on necessary layers and divs so that they appear
-   *
-   * @param {mapboxgl.Map} map - The map object for the page
-   */
   map.on("click", "all_pois", function (e) {
     var props = e.features[0].properties;
 
@@ -149,43 +184,25 @@ const poi_click = (map, graph) => {
 };
 
 const gap_click = (map) => {
-  /**
+  /*
+   * GAP layer is clickable below the ZOOM_THRESHOLD_FOR_LINE_INTERACTION
    */
   map.on("click", "gaps", function (e) {
-    var props = e.features[0].properties;
+    if (map.getZoom() > ZOOM_THRESHOLD_FOR_LINE_INTERACTION) {
+      var props = e.features[0].properties;
 
-    gap_click_logic(map, props.uid, e.lngLat, props.island_count);
+      gap_click_logic(map, props.uid, e.lngLat, props.island_count);
+    }
   });
 };
 
 const sw_click = (map) => {
+  /*
+   * SW layer is clickable below the ZOOM_THRESHOLD_FOR_LINE_INTERACTION
+   */
   map.on("click", "sw", function (e) {
-    var props = e.features[0].properties;
-
-    if (map.getZoom() > sw_zoom_threshold) {
-      reload_pois_near_sw(map, e.lngLat);
-
-      hide_osm_isos(map);
-      map.setFilter("selected_poi", ["==", "type", "none"]);
-
-      map.setPaintProperty("clicked_gap", "line-opacity", 0);
-
-      map.setFilter("selected_poi_entrypoints", [
-        "==",
-        "type",
-        "none - this filter should return zero results",
-      ]);
-
-      set_text_to_div("This existing sidewalk:", "gap-header");
-
-      hide_element("stat-island");
-
-      set_text_to_div("Around an Existing Sidewalk", "subtitle");
-
-      show_element("stat-box");
-      hide_element("info-box");
-      hide_element("walkshed-legend");
-      show_element("selected-legend");
+    if (map.getZoom() > ZOOM_THRESHOLD_FOR_LINE_INTERACTION) {
+      sw_click_logic(map, e);
     }
   });
 };
